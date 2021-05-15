0133864ae8ac7d6f4a59367f2f02f98eb7089e43
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2021-05-15 20:16:54 +0200

    Finished SpeedForce Calc

--- src/Calculations/SpeedForce.svelte
+++ src/Calculations/SpeedForce.svelte
@@ -1,9 +1,12 @@
 <script>
     // F = m * a
     let a=0,m=0;
+    $: f = m*a;
 </script>
 
+<h2>Siła ruchu jednostajnie przyśpieszona</h2>
+<h3>F = m * a</h3>
 
-Masa Ciała (m) : <input type="Number" value={m}/> <br/>
-Przyśpieszenie ciała(a) <input type="Number" value={a}/> <br/>
-Siła ruchu jednostajnie przyśpieszonym (F) <div></div> <br/>
+Masa Ciała (m) : <input type="Number" bind:value={m}/> <br/>
+Przyśpieszenie ciała(a) <input type="Number" bind:value={a}/> <br/>
+Siła ruchu jednostajnie przyśpieszonym (F) {f}<br/>
