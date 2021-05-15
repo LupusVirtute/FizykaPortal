4d4959a7784f8672adb8a67e39e8b72aa935ffe0
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2021-05-15 20:10:03 +0200

    feat: SpeedForce calculate file added

---
+++ src/Calculations/SpeedForce.svelte
@@ -0,0 +1,9 @@
+<script>
+    // F = m * a
+    let a=0,m=0;
+</script>
+
+
+Masa Ciała (m) : <input type="Number" value={m}/> <br/>
+Przyśpieszenie ciała(a) <input type="Number" value={a}/> <br/>
+Siła ruchu jednostajnie przyśpieszonym (F) <div></div> <br/>
