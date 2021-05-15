d2648843a216410a9300184c5d7f83eef76230dd
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2021-05-15 20:16:46 +0200

    Added AutoComponent rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+import SpeedForce from "./Calculations/SpeedForce.svelte";
+
+
+
+	let components = [
+		SpeedForce
+	];
+
 </script>
 
 <main>
 	<h1>Portal Fizyczny</h1>
+	{#each components as component}
+		<svelte:component this={component} />
+	{/each}
 </main>
 
 <style>
